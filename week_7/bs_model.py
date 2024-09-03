951b0b7e1f2b201d9b914f29a75174fd13515c5b
adevrent <a.devrent@hotmail.com>
2024-09-03 10:57:39 +0300

    heston and merton models MC complete

---
+++ week_7/bs_model.py
@@ -0,0 +1,40 @@
+import numpy as np
+import scipy.stats as ss
+
+def MC_price(S0, K, r, sigma, T, M, Ite, t, call_or_put="C"):
+    
+    T = T - t
+    dt = T / M
+    S = np.zeros((M+1, Ite))
+    S[0] = S0  # at t=0, all prices are S0. (first row is S0 repeated.)
+    rn = np.random.standard_normal((M, Ite))
+    multipliers = np.exp((r - sigma**2 / 2) * dt + sigma * np.sqrt(dt) * rn)
+    # print("multipliers.cumprod() =\n", multipliers.cumprod(axis=0))
+    S[1:, :] = S0 * multipliers.cumprod(axis=0)  # price paths are generated
+    print("S =\n", S)
+    
+    if call_or_put == "C":
+        average_payoff = np.maximum(S[-1, :] - K, 0).mean()
+    else:
+        average_payoff = np.maximum(K - S[-1, :], 0).mean()
+    
+    opt_price = np.exp(-r*dt) * average_payoff
+    return opt_price
+
+r = 0.05  # Risk-free rate
+sigma = 0.45  # Volatility
+T = 3/12  # Maturity/time period (in years)
+S0 = 200  # Current Stock Price
+
+M = 20000  # Number of simulations (paths)
+n = 1  # Number of steps
+
+# Option params
+K = 195
+t = 0
+
+# SEED
+np.random.seed(2)
+
+bs_opt_price = MC_price(S0, K, r, sigma, T, n, M, t, "C")
+print("bs_opt_price =", bs_opt_price)
---
+++ week_7/merton_model.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as ss
+
+def S(lamb, mu, delta, r, sigma, T, S0, n):
+    dt = T / n
+    r_j = lamb * (np.exp(mu + delta**2 / 2) - 1)
+    S_arr = np.zeros(n)
+    S_arr[0] = S0
+    for t in range(1, n):
+        S_arr[t] = np.maximum(0.0001, (S_arr[t-1] * (np.exp((r - r_j - sigma**2 / 2)*dt + sigma*np.sqrt(dt)*np.random.standard_normal(1)) + (np.exp(mu + delta * np.random.standard_normal(1)) - 1)*np.random.poisson(lamb*dt, 1))))
+    
+    return S_arr
+
+def merton_MC(M, lamb, mu, delta, r, sigma, T, S0, n):
+    S_MC = np.zeros((M, n))  # each row is one path's time series
+    for m in range(M):
+        S_MC[m, :] = S(lamb, mu, delta, r, sigma, T, S0, n)
+    
+    return S_MC
+
+def merton_opt_price_MC(merton_paramlist, K, t, call_or_put="C"):
+    M, lamb, mu, delta, r, sigma, T, S0, n = merton_paramlist
+    T_t = T - t
+    S_MC = merton_MC(M, lamb, mu, delta, r, sigma, T_t, S0, n)
+    if call_or_put == "C":
+        payoffs = np.maximum(0, S_MC[:, -1] - K)
+    else:
+        payoffs = np.maximum(0, K - S_MC[:, -1])
+    price = np.exp(-r * T_t) * payoffs.mean()
+    
+    return price
+
+lamb = 0.0  # Lambda of the model
+mu = 0.5  # Mu
+delta = 0.75  # Delta
+
+r = 0.05  # Risk-free rate
+sigma = 0.45  # Volatility
+T = 3/12  # Maturity/time period (in years)
+S0 = 200  # Current Stock Price
+
+M = 20000  # Number of simulations (paths)
+n = 90  # Number of steps
+
+merton_paramlist = [M, lamb, mu, delta, r, sigma, T, S0, n]
+
+# Option params
+K = 195
+t = 0
+
+# SEED
+np.random.seed(2)
+
+merton_opt_price = merton_opt_price_MC(merton_paramlist, K, t, "C")
+print("merton_opt_price =", merton_opt_price)
